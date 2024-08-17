357cf8241f6d57b6cc46e8e0886f813e75b8f0bf
Vinayak Prakash <vinayakprakash2121@gmail.com>
2024-08-17 22:46:51 +0530

    s

---
+++ RISC_V_RV32I/pipelined/decode_cycle.v
@@ -0,0 +1,115 @@
+module instruction_decode(clk, rst, IF_ID_IR, IF_ID_PC, IF_ID_PC1, RegWriteW, RDW, ResultW, RegWriteE, ALUSrcE, MemWriteE, ResultSrcE,
+    BranchE,  ALUControlE, RD1_E, RD2_E, Imm_Ext_E, RD_E, PCE, PCPlus4E, RS1_E, RS2_E);
+
+    // Declaring I/O
+    input clk, rst, RegWriteW;
+    input [4:0] RDW;
+    input [31:0] IF_ID_IR, IF_ID_PC, IF_ID_PC1, ResultW;
+    output RegWriteE,ALUSrcE,MemWriteE,ResultSrcE,BranchE;
+    output [2:0] ALUControlE;
+    output [31:0] RD1_E, RD2_E, Imm_Ext_E;
+    output [4:0] RS1_E, RS2_E, RD_E;
+    output [31:0] PCE, PCPlus4E;
+
+    // Declare Interim Wires
+    wire RegWriteD,ALUSrcD,MemWriteD,ResultSrcD,BranchD;
+    wire [1:0] ImmSrcD;
+    wire [2:0] ALUControlD;
+    wire [31:0] RD1_D, RD2_D, Imm_Ext_D;
+
+    // Declaration of Interim Register
+    reg RegWriteD_r,ALUSrcD_r,MemWriteD_r,ResultSrcD_r,BranchD_r;
+    reg [2:0] ALUControlD_r;
+    reg [31:0] RD1_D_r, RD2_D_r, Imm_Ext_D_r;
+    reg [4:0] RD_D_r, RS1_D_r, RS2_D_r;
+    reg [31:0] IF_ID_PC_r, IF_ID_PC1_r;
+
+
+    // Initiate the modules
+    // Control Unit
+    Control_Unit_Top control (
+                            .Op(IF_ID_IR[6:0]),
+                            .RegWrite(RegWriteD),
+                            .ImmSrc(ImmSrcD),
+                            .ALUSrc(ALUSrcD),
+                            .MemWrite(MemWriteD),
+                            .ResultSrc(ResultSrcD),
+                            .Branch(BranchD),
+                            .funct3(IF_ID_IR[14:12]),
+                            .funct7(IF_ID_IR[31:25]),
+                            .ALUControl(ALUControlD)
+                            );
+
+    // Register File
+    Register_File rf (
+                        .clk(clk),
+                        .rst(rst),
+                        .WE3(RegWriteW),
+                        .WD3(ResultW),
+                        .A1(IF_ID_IR[19:15]),
+                        .A2(IF_ID_IR[24:20]),
+                        .A3(RDW),
+                        .RD1(RD1_D),
+                        .RD2(RD2_D)
+                        );
+
+    // Sign Extension
+    Sign_Extend extension (
+                        .In(IF_ID_IR[31:0]),
+                        .Imm_Ext(Imm_Ext_D),
+                        .ImmSrc(ImmSrcD)
+                        );
+
+    // Declaring Register Logic
+    always @(posedge clk or posedge rst) begin
+        if(rst == 1'b1) begin
+            RegWriteD_r <= 1'b0;
+            ALUSrcD_r <= 1'b0;
+            MemWriteD_r <= 1'b0;
+            ResultSrcD_r <= 1'b0;
+            BranchD_r <= 1'b0;
+            ALUControlD_r <= 3'b000;
+            RD1_D_r <= 32'h00000000; 
+            RD2_D_r <= 32'h00000000; 
+            Imm_Ext_D_r <= 32'h00000000;
+            RD_D_r <= 5'h00;
+            IF_ID_PC_r <= 32'h00000000; 
+            IF_ID_PC1_r <= 32'h00000000;
+            RS1_D_r <= 5'h00;
+            RS2_D_r <= 5'h00;
+        end
+        else begin
+            RegWriteD_r <= RegWriteD;
+            ALUSrcD_r <= ALUSrcD;
+            MemWriteD_r <= MemWriteD;
+            ResultSrcD_r <= ResultSrcD;
+            BranchD_r <= BranchD;
+            ALUControlD_r <= ALUControlD;
+            RD1_D_r <= RD1_D; 
+            RD2_D_r <= RD2_D; 
+            Imm_Ext_D_r <= Imm_Ext_D;
+            RD_D_r <= IF_ID_IR[11:7];
+            IF_ID_PC_r <= IF_ID_PC; 
+            IF_ID_PC1_r <= IF_ID_PC1;
+            RS1_D_r <= IF_ID_IR[19:15];
+            RS2_D_r <= IF_ID_IR[24:20];
+        end
+    end
+
+    // Output asssign statements
+    assign RegWriteE = RegWriteD_r;
+    assign ALUSrcE = ALUSrcD_r;
+    assign MemWriteE = MemWriteD_r;
+    assign ResultSrcE = ResultSrcD_r;
+    assign BranchE = BranchD_r;
+    assign ALUControlE = ALUControlD_r;
+    assign RD1_E = RD1_D_r;
+    assign RD2_E = RD2_D_r;
+    assign Imm_Ext_E = Imm_Ext_D_r;
+    assign RD_E = RD_D_r;
+    assign PCE = IF_ID_PC_r;
+    assign PCPlus4E = IF_ID_PC1_r;
+    assign RS1_E = RS1_D_r;
+    assign RS2_E = RS2_D_r;
+
+endmodule
